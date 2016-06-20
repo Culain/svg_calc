0e1915f4cf9f24881fe8d3647c15b192536d74d5
lordfwahfnah <renegade2200@yahoo.de>
2016-06-20 23:16:04 +0200

    added calc_node.polyline()

--- cal.py
+++ cal.py
@@ -48,6 +48,9 @@ def calculate(filename):
     for line in lines:
         sum += calc_node.line(line)
 
+    for polyline in polylines:
+        sum += calc_node.polyline(polyline)
+
     return sum
 
     # try:
@@ -66,19 +69,35 @@ def get_dimensions(svgfile):
 
 class calc_node:
 
-    def line(node):
+    def line(self):
         x1 = 0
         x2 = 0
         y1 = 0
         y2 = 0
 
-        x1 = float(node.getAttribute('x1'))
-        y1 = float(node.getAttribute('y1'))
-        x2 = float(node.getAttribute('x2'))
-        y2 = float(node.getAttribute('y2'))
+        x1 = float(self.getAttribute('x1'))
+        y1 = float(self.getAttribute('y1'))
+        x2 = float(self.getAttribute('x2'))
+        y2 = float(self.getAttribute('y2'))
 
         line_length = tan(sqrt(abs(x2 - x1)**2 + abs(y2 - y1)**2))
         return line_length
 
-    def polyline(self, node):
-        pass
+    def polyline(self):  # <polyline class="fil0 str0" points="775,207 693,186 695,112 765,121 "/>
+        poly = self.getAttribute('points')
+        polysplit = poly.split(' ')[:-1]  # ['88,391', '21,398', '21,456', '80,456', '80,513', '26,500']
+        format_split = []
+        for split in polysplit:
+            split = split.split(',')  # ['286', '519']
+            temp = [float(split[0]), float(split[1])]  # [286.0, 519.0]
+            format_split.append(temp)
+
+        line_length = 0
+        for x in range(1, len(format_split)):
+            x1 = format_split[x-1][0]
+            y1 = format_split[x-1][1]
+            x2 = format_split[x][0]
+            y2 = format_split[x][1]
+            line_length += tan(sqrt(abs(x2 - x1)**2 + abs(y2 - y1)**2))
+
+        return line_length
